464a0ea61fcf2001a9363cb514ff7ba1ef5172fb
Sebastian Tschan <mail@blueimp.net>
2019-03-08 18:44:14 +0100

    Remove unnecessary shellcheck shell comment.
    Use -n instead of ! -z.

--- debsecan.sh
+++ debsecan.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# shellcheck shell=dash
 
 #
 # Uses debsecan to list CVEs for installed packages in a Debian Docker image.
@@ -17,7 +16,7 @@ set -e
 
 # Removes the temporary docker container:
 cleanup() {
-  if [ ! -z "$DEBSECAN_TARGET" ]; then
+  if [ -n "$DEBSECAN_TARGET" ]; then
     docker rm -vf "$DEBSECAN_TARGET" > /dev/null
   fi
 }
